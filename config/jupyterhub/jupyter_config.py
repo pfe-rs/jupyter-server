53af93032cc298644c30eca64980b1487b9077bc
Filip Parag <filip@parag.rs>
2021-10-20 02:02:53 +0200

    ansible jupyter

---
+++ config/jupyterhub/jupyter_config.py
@@ -0,0 +1,6 @@
+c.JupyterHub.port = 8001
+c.JupyterHub.proxy_api_port = 8003
+c.Authenticator.admin_users = {'pfe'}
+c.NotebookApp.allow_origin = '*'
+c.JupyterHub.admin_access = True
+c.NotebookApp.terminals_enabled = False
