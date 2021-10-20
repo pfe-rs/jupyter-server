f3bbed7e9026228397b52a99c2095a9c984c88ad
Luka Simić <git@kocka.tech>
2021-10-20 12:41:17 +0200

    Set up user creation.

---
+++ users.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+from json import dump, load
+from os.path import isfile
+from random import randint
+from typing import Optional
+
+def get_username(name: str) -> str:
+    name = name.lower()
+    name = name.replace(' ', '-')
+    name = name.replace('dž', 'dz')
+    name = name.replace('ž', 'z')
+    name = name.replace('ć', 'c')
+    name = name.replace('č', 'c')
+    name = name.replace('š', 'c')
+    name = name.replace('đ', 'dj')
+    return name
+
+users: Optional[list[dict[str, str]]] = None
+
+if isfile('users.json'):
+    with open('users.json', 'r', encoding='utf-8') as users_file:
+        users = load(users_file)
+else:
+    users = []
+
+if users is not None:
+    while True:
+        try:
+            name: str = input()
+            if len(name) == 0 or any(user['name'] == name for user in users):
+                continue
+            users.append({
+                'name': name,
+                'username': get_username(name),
+                'password': str(randint(1000000000, 9999999999))
+            })
+        except KeyboardInterrupt:
+            break
+        except EOFError:
+            break
+    with open('users.json', 'w', encoding='utf-8') as users_file:
+        dump(users, users_file, indent=4)
+    if isfile('/etc/dashboard.json'):
+        with open('/etc/dashboard.json', 'r+', encoding='utf-8') as dashboard_file:
+            dashboard: dict = load(dashboard_file)
+            dashboard['users'] = {}
+            for user in users:
+                dashboard['users'][user['username']] = user['name']
+            dashboard_file.seek(0)
+            dump(dashboard, dashboard_file, indent=4)
+            dashboard_file.truncate()
