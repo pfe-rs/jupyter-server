6678af22802b034d01f3729e1222e2c7569b0492
Filip Parag <filip@parag.rs>
2023-09-22 01:15:19 +0200

    quick read-only admin access

--- utils/jupyterhub_archive.sh
+++ utils/jupyterhub_archive.sh
@@ -5,8 +5,9 @@ archive="jupyterhub_$(date +%Y-%m-%dT%H:%M:%S).zip"
 
 compress_directory() {
     cd "/home" || exit 1
-    zip -r9P "$2" "$workdir/$1.zip"  "$1"/**
-    ls -la "$workdir/$1.zip"
+    echo "Data export for user $1 from '$archive'" > "$1/jupyterhub_export_info.txt"
+    zip -yrX9P "$2" "$workdir/$1.zip" "$1"/**
+    rm -f "$1/jupyterhub_export_info.txt"
 }
 
 jq -r '.[] | .username + "\t" + .password' < /etc/users.json | \
@@ -20,4 +21,4 @@ sh -c "cd $workdir && zip -r9 '$archive' . || exit 1"
 cp "$workdir/$archive" .
 rm -rf "$workdir"
 
-echo "Created archive: '$archive'"
+echo "Created archive: '$archive' ($(du -h "$archive" | cut -f1))"
