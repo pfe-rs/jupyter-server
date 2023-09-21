0b44fb3bc6987d667a00c8f643f44a20b36658b7
Filip Parag <filip@parag.rs>
2023-09-21 17:01:34 +0200

    use latest nodejs

--- config/jupyterhub/jupyterhub_config.py
+++ config/jupyterhub/jupyterhub_config.py
@@ -4,3 +4,5 @@ c.Authenticator.admin_users = {'pfe'}
 c.NotebookApp.allow_origin = '*'
 c.JupyterHub.admin_access = True
 c.NotebookApp.terminals_enabled = False
+c.LatexConfig.run_times = 2
+c.LatexConfig.latex_command = 'pdflatex'
